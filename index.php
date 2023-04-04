16feb71cef765ee2ed79c669fa1a17e0b02ed17b
Laura Carvalho <127751470+lauracar2@users.noreply.github.com>
2023-04-04 16:27:54 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <center>
+     <form id="enviar" action="media.php" method="POST">
+        nota 1: <input type="number" name="nota1">
+        <br>
+        nota 2: <input type="number" name="nota2">
+        <br>
+        nota 3: <input type="number" name="nota2">
+        <br>
+        <input type="submit" name="enviar" value="enviar">
+     </form>
+   </center>
+</body>
+</html>
